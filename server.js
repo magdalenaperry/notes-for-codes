29d3ede89a1a24fa2cc9cdd09b2c595b59a5dee7
Magdalena Perry <magperry16@gmail.com>
2022-03-24 15:38:50 -0400

    setting up code

---
+++ server.js
@@ -0,0 +1,33 @@
+// create my ports here and the listen to port
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// port should never change, which is why this is all caps--
+const PORT = 3001;
+
+// middlewear is what happens between our requsts and response servers
+// serves a static directory called 'public'
+// public just means that it is public
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+
+// serves up home apge, but this is not set to get sent to the index.html file
+// this is naturally set up to go to an html file in the public
+app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+
+
+// the apps ^ do not work unless this is listed
+// app listen is listening to the apps above
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
